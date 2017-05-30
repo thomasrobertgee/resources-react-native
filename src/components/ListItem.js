852b3d3699e7042fb501364967456217532fe883
Thomas Gee <thomasrobertgee@gmail.com>
2017-05-30 17:38:50 +1000

    added functionality that shows description when selected

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { Text, TouchableWithoutFeedback, View } from 'react-native'
 import { connect } from 'react-redux'
 import { CardSection } from './common'
 import * as actions from '../actions'
 
 class ListItem extends Component {
+
+  renderDescription() {
+    const { library, selectedLibraryId } = this.props
+    if (library.id === selectedLibraryId) {
+      return (
+        <Text>{library.description}</Text>
+      )
+    }
+  }
+
   render() {
     const { titleStyle } = styles
-    console.log(this.props)
+    const { id, title } = this.props.library
+
+
     return (
-      <CardSection>
-        <Text style={titleStyle}>
-          {this.props.library.title}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     )
   }
 }
@@ -25,4 +44,8 @@ const styles = {
   }
 }
 
-export default connect(null, actions)(ListItem)
+const mapStateToProps = state => {
+  return { selectedLibraryId: state.selectedLibraryId }
+}
+
+export default connect(mapStateToProps, actions)(ListItem)
